11f35031aefabc7c27bc7d0bae6249c33cfbbfd7
chenelias <elias9988110@gmail.com>
2023-01-01 19:07:45 +0800

    icon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,7 +17,7 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
                     rel="stylesheet"
                 ></link>
-                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="shortcut icon" href="https://www.eliaschen.dev/favicon.ico" />
                 <meta
                     name="description"
                     content="Learning about coding and creating something useful for every developers and user"
