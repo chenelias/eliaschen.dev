82495e8a44d1f2b7a41913dcb201d926f9a4ec0e
chenelias <elias9988110@gmail.com>
2023-01-03 19:05:01 +0800

    tab outline

--- pages/PinnedRepos.jsx
+++ pages/PinnedRepos.jsx
@@ -64,7 +64,7 @@ const PinnedRepos = () => {
     if (!data) return loadingdisplay
     var searchresult = data.map((repo) => (
         <Link className="group cursor-pointer block" target="_blank" href={repo.link}>
-            <div className="lg:h-[250px] bg-gradient-to-r from-purple-400 to-orange-500 dark:from-purple-500 dark:to-orange-700 w-full p-[4px] rounded-xl shadow-lg shodow-black-/5 dark:shadow-zinc-200/5 hover:shadow-xl hover:scale-[103%] dark:hover:shadow-zinc-200/20 hover:shadow-black/20 duration-300">
+            <div className="lg:h-[250px] bg-gradient-to-r from-purple-400 to-orange-500 dark:from-purple-500 dark:to-orange-700 w-full p-[4px] rounded-xl shadow-lg shodow-black-/5 dark:shadow-zinc-200/5 hover:shadow-xl hover:scale-[101%] dark:hover:shadow-zinc-200/20 hover:shadow-black/20 duration-300">
                 <div className="flex flex-col cursor-pointer gap-4 p-4 dark:bg-zinc-800 bg-slate-200 rounded-lg h-full">
                     <div>
                         <h1 className="dark:text-zinc-300 text-zinc-900 font-bold w-full tracking-tight text-2xl">
