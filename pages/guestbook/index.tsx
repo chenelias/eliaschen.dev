67787cb80c9fa782d7e3592acb53fbb01197c376
chenelias <elias9988110@gmail.com>
2023-01-17 17:31:25 +0800

    fix tsximport Body components

--- pages/guestbook/index.tsx
+++ pages/guestbook/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Body from '/components/Body'
+import Body from '../../components/Body'
 import { createClient } from '@supabase/supabase-js'
 export async function getStaticProps() {
     const supabaseAdmin = createClient(
