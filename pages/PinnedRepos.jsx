3381478c8721672134f3ebb41c0b520fe9e98d8a
chenelias <elias9988110@gmail.com>
2023-01-20 21:51:20 +0800

    Home page top-project card bg

--- pages/PinnedRepos.jsx
+++ pages/PinnedRepos.jsx
@@ -63,47 +63,48 @@ const PinnedRepos = () => {
     if (isLoading) return loadingdisplay
     if (!data) return loadingdisplay
     var searchresult = data.map((repo) => (
-        <Link
-            className="group cursor-pointer block"
-            target="_blank"
-            href={repo.link}
-            aria-label={'github repository ' + repo.repo}
-        >
-            <div className="lg:h-[250px] bg-gradient-to-r from-purple-400 to-orange-500 dark:from-purple-500 dark:to-orange-700 w-full p-[4px] rounded-xl shadow-lg shodow-black-/5 dark:shadow-zinc-200/5 hover:shadow-xl hover:scale-[101%] dark:hover:shadow-zinc-200/20 hover:shadow-black/20 duration-300">
-                <div className="flex flex-col cursor-pointer gap-4 p-4 dark:bg-zinc-800 bg-slate-200 rounded-lg h-full">
-                    <div>
-                        <h1 className="dark:text-zinc-300 text-zinc-900 font-bold w-full tracking-tight text-2xl">
-                            {repo.repo}
-                        </h1>
-                    </div>
-                    <div className=" lg:h-[130px]">
-                        <p className="line-clamp-4 ">{repo.description}</p>
-                    </div>
-
-                    <div className="dark:text-zinc-400 text-zinc-500 gap-2 text-base items-center font-semibold block">
-                        <div className="flex">
-                            <p className="items-center flex text-lg font-bold">
-                                <MdOutlineFavoriteBorder />
-                                &thinsp;{repo.stars}
-                            </p>
-                            &ensp;
-                            <p className="items-center flex text-lg font-bold">
-                                <BiGitRepoForked />
-                                &thinsp;{repo.forks}
-                            </p>
-                        </div>
-                        <div className="flex gap-2 items-center">
-                            <div
-                                style={{ backgroundColor: repo.languageColor }}
-                                className={` rounded-full h-[15px] w-[15px] border-1`}
-                            ></div>
-                            <p className=" inline-flex items-center">{repo.language}</p>
-                        </div>
-                    </div>
-                </div>
+      <Link
+        className="group cursor-pointer block"
+        target="_blank"
+        href={repo.link}
+        aria-label={"github repository " + repo.repo}
+      >
+        {/* from-purple-400 to-orange-500 dark:from-purple-500 dark:to-orange-700  */}
+        <div className="lg:h-[250px] bg-gradient-to-r from-orange-200 to-neutral-600 dark:from-orange-900 dark:to-neutral-600 w-full p-[4px] rounded-xl shadow-lg shodow-black-/5 dark:shadow-zinc-200/5 hover:shadow-xl hover:scale-[101%] dark:hover:shadow-zinc-200/20 hover:shadow-black/20 duration-300">
+          <div className="flex flex-col cursor-pointer gap-4 p-4 dark:bg-zinc-800 bg-slate-200 rounded-lg h-full">
+            <div>
+              <h1 className="dark:text-zinc-300 text-zinc-900 font-bold w-full tracking-tight text-2xl">
+                {repo.repo}
+              </h1>
             </div>
-        </Link>
-    ))
+            <div className=" lg:h-[130px]">
+              <p className="line-clamp-4 ">{repo.description}</p>
+            </div>
+
+            <div className="dark:text-zinc-400 text-zinc-500 gap-2 text-base items-center font-semibold block">
+              <div className="flex">
+                <p className="items-center flex text-lg font-bold">
+                  <MdOutlineFavoriteBorder />
+                  &thinsp;{repo.stars}
+                </p>
+                &ensp;
+                <p className="items-center flex text-lg font-bold">
+                  <BiGitRepoForked />
+                  &thinsp;{repo.forks}
+                </p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <div
+                  style={{ backgroundColor: repo.languageColor }}
+                  className={` rounded-full h-[15px] w-[15px] border-1`}
+                ></div>
+                <p className=" inline-flex items-center">{repo.language}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    ));
     return (
         <main>
             <div className="">
