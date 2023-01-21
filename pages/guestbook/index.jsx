9ee20c29f0c1b482f1bf402c96163bf646ceb318
chenelias <elias9988110@gmail.com>
2023-01-21 21:20:07 +0800

    add input style

--- pages/guestbook/index.jsx
+++ pages/guestbook/index.jsx
@@ -10,6 +10,7 @@ function GuestBookPage({ guestbook }) {
       provider: "github",
     });
   }
+  const messageinput = React.useRef();
   const [message, setmessage] = React.useState(null);
   const [username, setusername] = React.useState("eliaschen");
   const [date, setdate] = React.useState(null);
@@ -38,7 +39,7 @@ function GuestBookPage({ guestbook }) {
   //   }
   React.useEffect(() => {
     setloading(false);
-  }, [guestbook!==null]);
+  }, [guestbook !== null]);
   return (
     <Body title="GuestBook">
       <div className="mb-6">
@@ -58,51 +59,56 @@ function GuestBookPage({ guestbook }) {
         </div>
       </div>
       <div className="p-3 dark:bg-neutral-800 bg-neutral-200 drop-shadow-lg rounded-lg block mb-5">
-        <textarea
-          onChange={(x) => setmessage(x.target.value)}
-          name=""
-          id=""
-          cols={10}
-          rows={10}
-          className="p-2 w-full text-lg rounded-lg h-[130px] dark:bg-neutral-900 bg-neutral-100"
-        ></textarea>
+        <div className="flex">
+          <input
+            ref={messageinput}
+            onFocus={() => setmessagenull(false)}
+            onChange={(x) => setmessage(x.target.value)}
+            placeholder="Your message..."
+            type="text"
+            className="px-2 w-full text-lg rounded-lg h-10 dark:bg-neutral-900 bg-neutral-100"
+          ></input>
+          <button
+            onClick={() => {
+              !message ? setmessagenull(true) : setmessagenull(false);
+              getdate();
+              message && uploaddata();
+              messageinput.current.value = "";
+              // getServerSideProps();
+            }}
+            className="py-2 dark:bg-neutral-900 h-10 rounded-lg w-[140px] ml-2 hover:drop-shadow-md duration-100 bg-neutral-100"
+          >
+            Send It
+          </button>
+        </div>
         <p
-          className={`text-red-600 text-md font-bold ${
+          className={`text-red-600 text-md font-bold  ${
             messagenull ? (!message ? "block" : "hidden") : "hidden"
           }`}
         >
           Message is empty
         </p>
-        <div className="flex w-full">
-          <button
-            onClick={() => {
-              !message ? setmessagenull(true) : setmessagenull(false);
-              getdate();
-              uploaddata();
-              // getServerSideProps();
-            }}
-            className="p-2 dark:bg-neutral-900 rounded-lg w-full hover:drop-shadow-md duration-100 mt-3 bg-neutral-100"
-          >
-            Send It
-          </button>
-        </div>
       </div>
       <ul className="">
         {guestbook
-              .sort((a, b) => (a.id < b.id ? 1 : -1))
-              .map((guestbook) => (
-                <li
-                  key={guestbook.id}
-                  className="hover:bg-neutral-200 hover:dark:bg-neutral-800 hover:drop-shadow-lg duration-100 rounded-lg p-3 my-2"
-                >
-                  <p>{guestbook.message}</p>
-                  <div className="flex dark:text-zinc-600 text-zinc-500">
-                    <p>{guestbook.username}</p>
-                    <p className="mx-1">/</p>
-                    <p>{guestbook.date}</p>
-                  </div>
-                </li>
-              ))}
+          .sort((a, b) => (a.id < b.id ? 1 : -1))
+          .map((guestbook) => (
+            <li
+              key={guestbook.id}
+              className="hover:bg-neutral-200 hover:dark:bg-neutral-800 hover:drop-shadow-lg duration-100 rounded-lg p-3 my-2"
+            >
+              <p>{guestbook.message}</p>
+              <div className="flex dark:text-zinc-600 text-zinc-500">
+                <p>{guestbook.username}</p>
+                <p className="mx-1">/</p>
+                <p>{guestbook.date}</p>
+                <p className="mx-1">/</p>
+                <button className="text-red-600 hover:text-red-500 duration-100">
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
       </ul>
     </Body>
   );
