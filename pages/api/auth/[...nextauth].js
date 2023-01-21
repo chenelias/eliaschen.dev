2f26da6d52387dbc4b05ad7c58afe044482e009b
chenelias <elias9988110@gmail.com>
2023-01-22 04:53:40 +0800

    nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,6 +13,7 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    
   ],
   secret: process.env.JWT_SECRET,
 };
