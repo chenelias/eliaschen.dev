87ec401bdba3872027eb75f6930c76e9449e8fb2
eliaschen <eliaschen.dev@gmail.com>
2024-06-06 00:02:32 +0800

    fix links

--- components/data/MediaLinks.jsx
+++ components/data/MediaLinks.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-
 export const MediaLinks = [
-    {
-        name: 'Github',
-        username: 'chenelias',
-        url: 'https://github.com/chenelias/',
-        icon: 'logo-github',
-    },
-    {
-        name: 'DEV Community',
-        username: 'eliaschen',
-        url: 'https://dev.to/eliaschen/',
-        icon: 'code-outline',
-    },
-    {
-        name: 'X',
-        username: 'eliaschen_dev',
-        url: 'https://x.com/eliaschen_dev',
-        icon: 'logo-twitter',
-    },
-    {
-        name: 'Facebook',
-        username: 'eliaschen.dev',
-        url: 'https://www.facebook.com/people/Elias-Chen/100074943037854/',
-        icon: 'logo-facebook',
-    },
-    {
-        name: 'Instagram',
-        username: 'eliaschen.dev',
-        url: 'https://www.instagram.com/codechen9810/',
-        icon: 'logo-instagram',
-    },
-]
+  {
+    name: "Github",
+    username: "chenelias",
+    url: "https://github.com/chenelias/",
+    icon: "logo-github",
+  },
+  {
+    name: "DEV Community",
+    username: "eliaschen",
+    url: "https://dev.to/eliaschen/",
+    icon: "code-outline",
+  },
+  {
+    name: "X",
+    username: "eliaschen_dev",
+    url: "https://x.com/eliaschen_dev",
+    icon: "logo-twitter",
+  },
+  {
+    name: "Facebook",
+    username: "eliaschen.dev",
+    url: "https://www.facebook.com/eliaschen.dev/",
+    icon: "logo-facebook",
+  },
+  {
+    name: "Instagram",
+    username: "eliaschen.dev",
+    url: "https://www.instagram.com/eliaschen.dev/",
+    icon: "logo-instagram",
+  },
+];
